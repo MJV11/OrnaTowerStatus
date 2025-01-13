3ccba309e3ed18a449b9bc3d60b483fe0c7af3af
Max Vink'git config --global user.name Max <max.vink@berkeley.edu>
2025-01-13 09:39:29 -0800

    updated descriptions

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Celestial Tower Floor Status</h1>
+      <h1>Orna Tower Floor Status</h1>
       <div className="TowerBox">
         <TowerList />
       </div>
-      <p>Skipping 10 Floors Yields 98%. Skipping 20 Floors Yields 91%. Skipping 30 Floors Yields 79%. Skipping 40 Floors Yields 64%.</p>
+      <p>Skipping 10 Floors Yields -1k Shards. Skipping 20 Floors Yields -3k Shards. Skipping 30 Floors Yields -6.5k Shards. Skipping 40 Floors Yields -11k Shards.</p>
 
     </div>
   );
